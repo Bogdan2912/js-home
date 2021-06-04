9645be712432faebb83aed098fa334ac951ed70e
Bogdan2912 <super.bogdan68@gmail.com>
2021-06-04 18:04:45 +0300

    home2

---
+++ hw 2/main.js
@@ -0,0 +1,93 @@
+//Задание 1
+var quotient = 66 / 33;
+console.log(quotient);
+//Задание 2
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+console.log(myVar);
+//Задание 3
+var myExample = 11;
+
+// Only change code below this line
+myExample--;
+console.log(myExample);
+//Задание 4
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
+console.log(myDecimal);
+//Задание 5
+var product = 2.0 * 2.5;
+console.log(product);
+//Задание 6
+var quotient = 4.4 / 2.0; 
+console.log(quotient);
+//Задание 7
+var remainder = 11 % 3;
+console.log(remainder);
+//Задание 8
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+console.log(a);
+console.log(b);
+console.log(c);
+//Задание 9
+var d = 11;
+var e = 9;
+var f = 3;
+
+// Only change code below this line
+d -=6;
+e -=15;
+f -=1;
+console.log(d);
+console.log(e);
+console.log(f);
+//Задание 10
+var h = 5;
+var j = 12;
+var k = 4.6;
+
+// Only change code below this line
+h *= 5;
+j *= 3;
+k *= 10;
+console.log(h);
+console.log(j);
+console.log(k);
+//Задание 11
+var l = 48;
+var m = 108;
+var n = 33;
+
+// Only change code below this line
+l /= 12;
+m /= 4;
+n /= 11;
+console.log(l);
+console.log(m);
+console.log(n);
+//Задание 12
+var myFirstName = "Bogdan";
+var myLastName = "Danil";
+console.log(myFirstName);
+console.log(myLastName);
+//Задание 13
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+console.log(myStr);
+//Задание 14
+var myStr1 = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr1);
+//Задание 15
+var myStr2 = "FirstLine\n\t\\SecondLine\nThirdLine"; 
+console.log(myStr2);
